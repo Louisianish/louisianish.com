98ff564b3db066a9d575d840bfcc25a35071c15c
Louisianish <84807582+Louisianish@users.noreply.github.com>
2022-01-28 04:42:08 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,9 +35,7 @@
 		if (window.innerWidth < 1000) {
 			$(window).scroll(function() {
 				var scrolledY = $(window).scrollTop(), speed = 1;
-				// $('.wrapper').css('background-position', 'left ' + ((scrolledY)) + 'px');
 				$('.wrapper').css('background-position', 'left ' + ((scrolledY * speed)) + 'px');
-				// $('.background-image').css('background-attachment', 'fixed');
 			});
 		}
 
